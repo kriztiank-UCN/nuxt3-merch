9a7383a7f6b6124a3541ec942c3b3b41a87c9a16
Kristian <10221425@ucn.dk>
2023-09-22 17:10:34 +0200

    Fetching Data

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-    <p>Products</p>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="p in products">
+        <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
 
+<!-- script setup = composition API useFetch -->
 <script setup>
+//  fetch the products
+const { data: products } = await useFetch('https://fakestoreapi.com/products')
 // nuxt composable function
-  definePageMeta({
-    layout: "products",
-  })
+definePageMeta({
+  layout: 'products',
+})
 </script>
