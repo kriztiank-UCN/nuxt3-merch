12b4d6d7274086f7dc5bf9302b3adbe6e67d7176
Kristian <10221425@ucn.dk>
2023-09-22 14:22:42 +0200

    adding pages & route parameters

---
+++ pages/products/[id].vue
@@ -0,0 +1,15 @@
+<!-- Route Parameters /products/123 -->
+<!-- capture id with the use route composable function -->
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste necessitatibus accusamus
+      architecto dignissimos non harum tenetur rem itaque, dolor aut?
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+</script>
