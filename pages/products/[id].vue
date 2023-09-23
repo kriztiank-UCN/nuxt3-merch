1263e157290b87af60c2077b9d8ecbb678dfd522
Kristian <10221425@ucn.dk>
2023-09-23 06:31:09 +0200

    Error Page

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -8,13 +8,18 @@
 </template>
 
 <script setup>
-  const { id } = useRoute().params
-  const uri = 'https://fakestoreapi.com/products/' + id
+const { id } = useRoute().params
+const uri = 'https://fakestoreapi.com/products/' + id
 
-  //  fetch the products
-  const { data: product } = await useFetch(uri, { key: id })
+//  fetch the products
+const { data: product } = await useFetch(uri, { key: id })
 
-  definePageMeta({
-    layout: "products",
-  })
+//  throw createError & output in error.vue
+if (!product.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Product not found', fatal: true })
+}
+
+definePageMeta({
+  layout: 'products',
+})
 </script>
